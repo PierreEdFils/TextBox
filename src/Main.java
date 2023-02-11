ce52d687d8bc16c39eefcc2709bb902f0160c221
Pierre Fils <epierrefils@gmail.com>
2023-02-10 23:42:00 -0500

    Creating objects in  TextBox project

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        var textBox1 = new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text.toUpperCase());
 
-        System.out.println("Hello world!");
+        var textBox2 = new TextBox();
+        textBox2.setText("Box 2");
+        System.out.println(textBox2.text);
+//        var textBox2 = textBox1;
+//        textBox2.setText("Hello World");
+//        System.out.println(textBox1.text);
     }
 }
